2ab2765107428f9b41e2d551eb69d5926c4ff283
mesadastudent <92982517+mesadastudent@users.noreply.github.com>
2026-03-25 17:59:25 +0800

    checkout responsive

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-const trustbox = document.getElementById('trustbox');
-window.Trustpilot.loadFromElement(trustbox);
+document.addEventListener('DOMContentLoaded', () => {
+    const billing = document.querySelectorAll('input[name="billing"]');
+    const diffFields = document.getElementById('diffFields');
+
+    billing.forEach(opt => {
+        opt.addEventListener('change', () => {
+            if (opt.value === "different") {
+                diffFields.style.display = "block";
+            } else {
+                diffFields.style.display = "none";
+            }
+        });
+    });
+
+    const payment = document.querySelectorAll('input[name="payment"]');
+    const creditFields = document.getElementById('creditFields');
+
+    payment.forEach(opt => {
+        opt.addEventListener('change', () => {
+            if (opt.value === "credit") {
+                creditFields.style.display = "block";
+            } else {
+                creditFields.style.display = "none";
+            }
+        });
+    });
+
+});
