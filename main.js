824f6a10f349fb82f959c94fbc7a3d59de9cc7e8
mesadastudent <92982517+mesadastudent@users.noreply.github.com>
2026-03-26 18:13:50 +0800

    checkout improvement v1

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const billing = document.querySelectorAll('input[name="billing"]');
     const diffFields = document.getElementById('diffFields');
 
@@ -23,6 +24,42 @@ document.addEventListener('DOMContentLoaded', () => {
                 creditFields.style.display = "none";
             }
         });
+    });
+
+    const form = document.getElementById("checkoutForm");
+
+    if (!form) return;
+
+    form.addEventListener("submit", function (e) {
+
+        let fields = document.querySelectorAll(".field input");
+        let valid = true;
+
+        fields.forEach(field => {
+
+            let error = field.nextElementSibling;
+
+            if (field.value.trim() === "") {
+
+                field.classList.add("error");
+                error.style.display = "block";
+                error.textContent = "This field is required";
+
+                valid = false;
+
+            } else {
+
+                field.classList.remove("error");
+                error.style.display = "none";
+
+            }
+
+        });
+
+        if (!valid) {
+            e.preventDefault();
+        }
+
     });
 
 });
